2d4339596ae75fc48db0d0e69d3fb5bc11086440
Lucrezia Valeriani <valerianilucrezia11@gmail.com>
2024-09-19 14:20:30 +0200

    Update run_sequencing.sh

--- simulations_rRACES/run_sequencing.sh
+++ simulations_rRACES/run_sequencing.sh
@@ -2,7 +2,6 @@
 #SBATCH --partition=THIN
 #SBATCH --job-name=seq_races
 #SBATCH --nodes=1
-###SBATCH --ntasks=1
 #SBATCH --array=1-30
 #SBARCH --cpus-per-task=6
 #SBATCH --mem 30g
@@ -10,9 +9,9 @@
 #SBATCH --output=seq_%a.out
 
 sim=${SLURM_ARRAY_TASK_ID}
-#sim=$(($s + 10))
 echo $sim
 
+OUTDIR=""
+
 module load R
-Rscript 3_sim_seq.R --simulation "sim_${sim}"
-#Rscript 4_smoothing.R --simulation "sim_${sim}"
+Rscript 3_sim_seq.R --simulation "sim_${sim}" --type clonal --path $OUTDIR
